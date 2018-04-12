5b63e68e2b58ac818d42c4a9928e8fb66e2626cc
MOAIZ AHMAD <S331389056@OCDSB>
2018-04-12 11:04:35 -0400

    Flash logo

--- logo.py
+++ logo.py
@@ -1,6 +1,6 @@
 # ICS3U
 # Assignment 2: Logo
-# <your name>
+# <MOAIZ AHMAD>
 
 # adapted from http://www.101computing.net/getting-started-with-pygame/
 
@@ -13,8 +13,8 @@ pygame.init()
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
-RED = (255, 0, 0)
-
+RED = (224, 53, 53)
+yellow = (227,221,71)
 # Set the screen size (please don't change this)
 SCREENWIDTH = 400
 SCREENHEIGHT = 400
@@ -44,12 +44,21 @@ while carryOn:
     # --- Draw code goes here
 
     # Clear the screen to white
-    screen.fill(WHITE)
+    screen.fill(RED)
 
     # Queue different shapes and lines to be drawn
     # pygame.draw.rect(screen, RED, [55, 200, 100, 70], 0)
-    # pygame.draw.line(screen, GREEN, [0, 0], [100, 100], 5)
-    # pygame.draw.ellipse(screen, BLACK, [20, 20, 250, 100], 2)
+    pygame.draw.ellipse(screen, yellow, [100, 100, 200, 200], 0)#The outer yellow circle
+    pygame.draw.ellipse(screen, BLACK, [100, 100, 200, 200], 4)#The outer outline
+    pygame.draw.ellipse(screen, WHITE, [125, 125, 150, 150], 0)#The inner most circle 
+    pygame.draw.ellipse(screen, BLACK, [125, 125, 150, 150], 4)#The border around the white circle
+    pygame.draw.polygon(screen, yellow,[[135,200],[300,40],[200,200]])#the top of the lightning bolt
+    pygame.draw.polygon(screen, BLACK,[[135,200],[300,40],[200,200]],4)#the top border of the lightning bolt
+    pygame.draw.polygon(screen, yellow,[[265,180],[100,300],[200,180]])#The lower lightning bolt
+    pygame.draw.polygon(screen, BLACK,[[265,180],[100,300],[200,180]],4)#The lower border of the lightning bolt
+    pygame.draw.line(screen, yellow,[200,178],[185,198],6)
+    
+        
 
     # Update the screen with queued shapes
     pygame.display.flip()
